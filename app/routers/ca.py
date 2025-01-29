1903fdf19a14666e2cdb1a1b1079aebd82273f71
Samy MOSA <samymosa02@gmail.com>
2025-01-29 18:31:48 +0100

    refactor: Automate meuch_map generation

--- app/routers/ca.py
+++ app/routers/ca.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from app.services import va
 
@@ -9,8 +10,16 @@ router = APIRouter(
 )
 
 
-@router.post("/")
-async def ca(debut: date = date.min, fin: date = date.max):
+class CAResponse(BaseModel):
+    ca: float
+
+
+@router.get(
+    "",
+    description="Récupérer le chiffre d'affaires sur une période donnée",
+    response_model=CAResponse,
+)
+async def ca(debut: date = date.min, fin: date = date.max) -> dict:
     ventes = va.ventes(debut, fin)
     ca = sum(vente["total"] for vente in ventes)
     return {"ca": ca}
--- app/routers/tva.py
+++ app/routers/tva.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from app.services import va
 
@@ -9,7 +10,15 @@ router = APIRouter(
 )
 
 
-@router.post("/")
+class TVAResponse(BaseModel):
+    tva: float
+
+
+@router.get(
+    "",
+    description="Calculer la TVA à déclarer sur une période donnée",
+    response_model=TVAResponse,
+)
 async def tva(debut: date = date.min, fin: date = date.max):
     tx_tva = 0.2
     total_ventes = sum(vente["total"] for vente in va.ventes(debut, fin))
